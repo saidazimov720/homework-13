6192d79285d026eac4b97966b122fa37c7614ef0
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-27 17:07:48 +0500

    create the element and add the src

--- image_shower/script.js
+++ image_shower/script.js
@@ -1,5 +1,12 @@
 function displayImage() {
     const Url = document.getElementById("Url").value;
     const image = document.getElementById("image");
-    
+
+    if (Url) {
+        const img = document.createElement('img');
+        img.src = Url;
+        img.alt = `Bu yerda reklamangiz bo'lishi mumkin edi`;
+    } else {
+        
+    }
 }
