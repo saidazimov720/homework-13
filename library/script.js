75cc9aa9393ba0f83a65e3d9154375d1b253b196
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-27 19:20:38 +0500

    name add function

--- library/script.js
+++ library/script.js
@@ -1,5 +1,12 @@
 let names = [];
-const inputName = document.getElementById("inputName");
+const inputName = document.getElementById("inputName").value.trim();
 const nameList = document.getElementById("nameList");
 const nameSearch = document.getElementById("nameSearch");
 
+function nameAdd() {
+    if (inputName) {
+        names.push(inputName);
+        inputName.value = '';
+        displayNames();
+    }
+}
