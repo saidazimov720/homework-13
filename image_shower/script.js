3c2ded6cc35d559323d2c108c9f4104c36b8acf1
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-27 17:12:35 +0500

    fisrt test

--- image_shower/script.js
+++ image_shower/script.js
@@ -6,7 +6,12 @@ function displayImage() {
         const img = document.createElement('img');
         img.src = Url;
         img.alt = `Bu yerda reklamangiz bo'lishi mumkin edi`;
+        img.classList.add('image_display');
+        const container = document.createElement('div');
+        container.classList.add('image_wrapper');
+        container.appendChild(img);
+        image.appendChild(container);
     } else {
-        
+        alert("Invalid Url")
     }
 }
